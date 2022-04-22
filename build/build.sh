3e9f99b9bf0db1f01afbf3cd35525a300cd23a81
N0rmal8e <normalbe@163.com>
2022-04-22 23:01:28 +0800

    update output string in build.sh

--- build/build.sh
+++ build/build.sh
@@ -21,8 +21,11 @@ CGO_ENABLED=0 gox -cgo=0 -osarch="linux/arm64" $PROJECTPATH/cmd/checksec/...
 upx $PROJECTPATH/build/bin/release/*
 
 echo "Done !"
-echo "You can find the binary files in ${PROJECTPATH}/build/bin/release/ in the docker\n or in the project folder ./build/bin/release"
+echo "You can find the binary files in ${PROJECTPATH}/build/bin/release/ folder in the docker"
+echo "or in the project's ./build/bin/release/ folder"
 
+sleep 1
+echo "ls -alh ${PROJECTPATH}/build/bin/release/ (in the docker) or ls -alh ./build/bin/release/ (in the host): "
 ls -alh
 
 # Done
