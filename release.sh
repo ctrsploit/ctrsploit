e69a10b9d299a90031ae1beab9fcf401406e4c56
LEI WANG <ssst0n3@gmail.com>
2023-08-03 12:23:51 +0800

    ctrsploit/env: split a single binary

--- release.sh
+++ release.sh
@@ -6,5 +6,6 @@ mkdir -p bin/release
 cd bin/release
 CGO_ENABLED=0 gox -cgo=0 -osarch="linux/amd64" -osarch="linux/arm64" -ldflags "${LDFLAGS}" github.com/ctrsploit/ctrsploit/cmd/ctrsploit
 CGO_ENABLED=0 gox -cgo=0 -osarch="linux/amd64" -osarch="linux/arm64" -ldflags "${LDFLAGS}" github.com/ctrsploit/ctrsploit/cmd/checksec
+CGO_ENABLED=0 gox -cgo=0 -osarch="linux/amd64" -osarch="linux/arm64" -ldflags "${LDFLAGS}" github.com/ctrsploit/ctrsploit/cmd/env
 cd -
 upx bin/release/*
