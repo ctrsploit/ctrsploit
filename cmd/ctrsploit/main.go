e7506cf9ff69d33f225eb7bcafdac207d4b9c602
LEI WANG <ssst0n3@gmail.com>
2023-09-22 17:55:09 +0800

    add colorful for ctrsploit too

--- cmd/ctrsploit/main.go
+++ cmd/ctrsploit/main.go
@@ -5,15 +5,11 @@ import (
 	"github.com/ctrsploit/ctrsploit/cmd/ctrsploit/env"
 	"github.com/ctrsploit/ctrsploit/cmd/ctrsploit/exploit"
 	"github.com/ctrsploit/ctrsploit/cmd/ctrsploit/helper"
+	"github.com/ctrsploit/ctrsploit/internal"
 	"github.com/ctrsploit/ctrsploit/internal/log"
 	"github.com/ctrsploit/ctrsploit/version"
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/sirupsen/logrus"
-	"github.com/ssst0n3/awesome_libs/awesome_error"
-	"github.com/ssst0n3/awesome_libs/awesome_error/exporter"
-	log2 "github.com/ssst0n3/awesome_libs/log"
 	"github.com/urfave/cli/v2"
-	"io"
 	"os"
 )
 
@@ -37,35 +33,8 @@ func main() {
 			helper.Command,
 			version.Command,
 		},
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "lang",
-				Value: "english",
-				Usage: "language for the greeting",
-			},
-			&cli.BoolFlag{
-				Name:  "debug",
-				Value: false,
-				Usage: "Output information for helping debugging ctrsploit",
-			},
-		},
-		Before: func(context *cli.Context) (err error) {
-			debug := context.Bool("debug")
-			awesome_error.Default = exporter.GetAwesomeError(log.Logger, debug)
-			if !debug {
-				log2.Logger.SetOutput(io.Discard)
-			} else {
-				log.Logger.Level = logrus.DebugLevel
-				log.Logger.SetReportCaller(true)
-				log.Logger.SetFormatter(&logrus.TextFormatter{
-					ForceColors: true,
-				})
-				log2.Logger.Level = logrus.DebugLevel
-				log2.Logger.Debug("debug mode on")
-			}
-			return
-		},
 	}
+	internal.InstallGlobalFlags(app)
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Logger.Fatal(err)
