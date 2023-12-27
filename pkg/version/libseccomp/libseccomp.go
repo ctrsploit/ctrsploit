ecc4610fb79a9ec58eb6c5c84947c60c4eb9081f
LEI WANG <ssst0n3@gmail.com>
2023-12-27 15:58:37 +0800

    update versions
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- pkg/version/libseccomp/libseccomp.go
+++ pkg/version/libseccomp/libseccomp.go
@@ -1,5 +1,7 @@
 package libseccomp
 
+import "github.com/ctrsploit/ctrsploit/pkg/version/version"
+
 // Version https://man7.org/linux/man-pages/man3/seccomp_version.3.html
 type Version struct {
 	Major int
@@ -7,22 +9,18 @@ type Version struct {
 	Micro int
 }
 
+func NewMap(versions []string) (m version.Map) {
+	m = version.Map{}
+	for _, v := range versions {
+		m[v] = New(v)
+	}
+	return
+}
+
 func New(version string) Version {
 	return Version{}
 }
 
-type Map map[string]Version
-type Slice []Version
-
-var (
-	Versions = Map{
-		"2.3.3": New("2.3.3"),
-		"2.4.1": New("2.4.1"),
-		"2.5.1": New("2.5.1"),
-	}
-	Order = Slice{
-		Versions["2.3.3"],
-		Versions["2.4.1"],
-		Versions["2.5.1"],
-	}
-)
+func (v Version) String() (version string) {
+	return
+}
--- pkg/version/runc/release.go
+++ pkg/version/runc/release.go
@@ -8,13 +8,13 @@ var (
 			Url:        "",
 			Releaser:   GithubRelease,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.4.1"],
+			LibSeccomp: libseccomp.Versions["2.4.1"].(libseccomp.Version),
 		},
 		"1.0.0-rc93": {
 			Url:        "https://github.com/opencontainers/runc/releases/download/v1.0.0-rc93/runc.amd64",
 			Releaser:   GithubRelease,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 		},
 	}
 	DindVersions = Versions{
@@ -22,14 +22,14 @@ var (
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.3.3"],
+			LibSeccomp: libseccomp.Versions["2.3.3"].(libseccomp.Version),
 			Note:       "",
 		},
 		"1.0.0-rc93": {
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.3.3"],
+			LibSeccomp: libseccomp.Versions["2.3.3"].(libseccomp.Version),
 			Note: `
 docker:20.10.4-dind
 docker:20.10.5-dind`,
@@ -38,14 +38,14 @@ docker:20.10.5-dind`,
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.4.4"],
+			LibSeccomp: libseccomp.Versions["2.4.4"].(libseccomp.Version),
 			Note:       `docker:20.10.6-dind`,
 		},
 		"1.1.4": {
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note: `
 docker:20.10.23-dind
 docker:23.0.0-rc.4-dind
@@ -57,7 +57,7 @@ docker:23.0.2-dind`,
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note: `
 docker:20.10.24-dind
 docker:23.0.3-dind
@@ -70,14 +70,14 @@ docker:24.0.0-beta.2-dind`,
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note:       `docker:24.0.0-rc.1-dind`,
 		},
 		"1.1.7": {
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note: `
 docker:23.0.6-dind
 docker:24.0.0-rc.2-dind
@@ -93,14 +93,14 @@ docker:24.0.4-dind`,
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note:       `docker:24.0.5-dind`,
 		},
 		"1.1.9": {
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note: `
 docker:24.0.6-dind
 docker:24.0.7-dind`,
@@ -109,7 +109,7 @@ docker:24.0.7-dind`,
 			Url:        "",
 			Releaser:   DockerhubDind,
 			Static:     true,
-			LibSeccomp: libseccomp.Versions["2.5.1"],
+			LibSeccomp: libseccomp.Versions["2.5.1"].(libseccomp.Version),
 			Note:       `docker:25.0.0-beta.1`,
 		},
 	}
