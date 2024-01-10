305978b3f98272369f93a48baf187aae4bb3b2af
LEI WANG <ssst0n3@gmail.com>
2024-01-10 11:52:42 +0800

    fix interface conversion: version.Version is *libseccomp.Version, not libseccomp.Version
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- pkg/version/libseccomp/libseccomp.go
+++ pkg/version/libseccomp/libseccomp.go
@@ -4,21 +4,21 @@ import "github.com/ctrsploit/ctrsploit/pkg/version/version"
 
 // Version https://man7.org/linux/man-pages/man3/seccomp_version.3.html
 type Version struct {
-	Major int
-	Minor int
-	Micro int
+	version.Number
 }
 
 func NewMap(versions []string) (m version.Map) {
 	m = version.Map{}
 	for _, v := range versions {
-		m[v] = New(v)
+		m[v] = *New(v)
 	}
 	return
 }
 
-func New(version string) Version {
-	return Version{}
+func New(v string) *Version {
+	return &Version{
+		Number: *version.New(v),
+	}
 }
 
 func (v Version) String() (version string) {
