2a7ad29fd522120603bf18fa7bced379ea06ac20
LEI WANG <ssst0n3@gmail.com>
2024-01-16 18:21:36 +0800

    update get library_version command
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- hack/version_collect/runc_collector/cmd/runc_collector/dind_test.go
+++ hack/version_collect/runc_collector/cmd/runc_collector/dind_test.go
@@ -1,9 +1,21 @@
 package main
 
-import "testing"
+import (
+	"github.com/ctrsploit/sploit-spec/pkg/log"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 // TestDind
 // test: sudo -E /home/st0n3/sdk/go1.21.5/bin/go test -test.run ^TestDind$ -timeout 99999s
 func TestDind(t *testing.T) {
 	Dind()
+}
+
+// TestImageLibseccompVersion
+// test: sudo -E /home/st0n3/sdk/go1.21.5/bin/go test -test.run ^TestImageLibseccompVersion$
+func TestImageLibseccompVersion(t *testing.T) {
+	ver, err := ImageLibseccompVersion("docker:19.03.13-dind")
+	assert.NoError(t, err)
+	log.Logger.Info(ver)
 }
