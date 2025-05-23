e0492182d735fd78371b7956218ddef1c75f05ac
LEI WANG <ssst0n3@gmail.com>
2025-05-23 11:12:47 +0800

    Optimize for cve-2020-15257
    1. fix a exploit method selection bug
    2. update vul's readme
    
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- cmd/ctrsploit/checksec/checksec.go
+++ cmd/ctrsploit/checksec/checksec.go
@@ -20,6 +20,6 @@ var Command = &cli.Command{
 		app.Vul2ChecksecCmd(&sys_admin.SysadminCgroupV1, []string{"sys_admin", "release_agent", "ra"}, nil),
 		app.Vul2ChecksecCmd(&vul.NetworkNamespaceHostLevel, []string{"host"}, nil),
 		app.Vul2ChecksecCmd(&shocker.Shocker, []string{"cap_dac_read_search", "open_by_handle_at"}, nil),
-		app.Vul2ChecksecCmd(&cve_2020_15257.Vul, []string{"15257"}, nil),
+		cve_2020_15257.CheckSecCmd,
 	},
 }
