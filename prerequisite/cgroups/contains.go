912cb4009b18fb7f3d12124b6cba9bc47ed766e9
LEI WANG <ssst0n3@gmail.com>
2025-10-01 19:03:42 +0800

    [feat] add a pre: check runtime type by /proc/1/cgroup
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

---
+++ prerequisite/cgroups/contains.go
@@ -0,0 +1,56 @@
+package cgroups
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ctrsploit/sploit-spec/pkg/exeenv"
+	"github.com/ctrsploit/sploit-spec/pkg/prerequisite"
+)
+
+type Contains struct {
+	prerequisite.BasePrerequisite
+	Expected string
+}
+
+func (p *Contains) Check() (bool, error) {
+	if p.Checked {
+		return p.Satisfied, nil
+	}
+	file, err := os.Open("/proc/1/cgroup")
+	if err != nil {
+		return false, fmt.Errorf("failed to check %s caused by reading /proc/1/cgroup: %w", p.Name, err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 0 && line[0] != '0' {
+			p.Satisfied = strings.Contains(line, p.Expected)
+			if p.Satisfied {
+				break
+			}
+		}
+	}
+	p.Checked = true
+	return p.Satisfied, nil
+}
+
+var (
+	// ContainsDocker check cgroupsPath contains /docker
+	//parent default do docker when use cgroupfs
+	//https://github.com/moby/moby/blob/v28.4.0/daemon/oci_linux.go#L779
+	//2:cpu,cpuacct:/docker/2403ccfe427f1dc7b6120dccb9ac58c2f8456950380d587a9f33ea7f00dd303d
+	//1:name=systemd:/docker/2403ccfe427f1dc7b6120dccb9ac58c2f8456950380d587a9f33ea7f00dd303d
+	//0::/system.slice/containerd.service
+	ContainsDocker = Contains{
+		BasePrerequisite: prerequisite.BasePrerequisite{
+			Name:   "/proc/1/cgroup",
+			Info:   "/proc/1/cgroup contains /docker",
+			ExeEnv: exeenv.InContainer,
+		},
+		Expected: "/docker",
+	}
+)
