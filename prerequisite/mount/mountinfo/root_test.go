42cbd73bcd1fe3b36ff5b01502f3b1371c68e4a0
LEI WANG <ssst0n3@gmail.com>
2025-10-04 21:27:32 +0800

    e2e: k8s testcase for checking /etc/hostname mountinfo
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- prerequisite/mount/mountinfo/root_test.go
+++ prerequisite/mount/mountinfo/root_test.go
@@ -28,7 +28,22 @@ func TestE2E_HostsRootContainsDocker(t *testing.T) {
 	})
 }
 
-// TODO: Test By kubernetes-v1.33-calico
-// but how to volume ctrsploit.test gracefully?
 func TestE2E_HostnameRootContainsContainerd(t *testing.T) {
+	tests := map[string]struct {
+		satisfied bool
+	}{
+		"kubernetes-v1.33.1-calico": {
+			satisfied: true,
+		},
+	}
+	testEnv := os.Getenv("TEST_ENV")
+	test, ok := tests[testEnv]
+	if !ok {
+		t.Skipf("Skipping test for unsupported environment: %s", testEnv)
+	}
+	t.Run(fmt.Sprintf("%s", testEnv), func(t *testing.T) {
+		satisfied, err := HostnameRootContainsContainerd.Check()
+		assert.NoError(t, err)
+		assert.Equal(t, test.satisfied, satisfied)
+	})
 }
