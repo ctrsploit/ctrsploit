4ac7a3d8831dded7b9d91f8c952531b2b6855e15
LEI WANG <ssst0n3@gmail.com>
2025-10-04 23:57:01 +0800

    feat(pre): check runtime type by apparmor profile
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- prerequisite/apparmor/profile.go
+++ prerequisite/apparmor/profile.go
@@ -9,12 +9,12 @@ import (
 	"github.com/ctrsploit/sploit-spec/pkg/prerequisite"
 )
 
-type ProfileNameEqualTo struct {
+type ProfileNameContains struct {
 	prerequisite.BasePrerequisite
-	Name string
+	Expected string
 }
 
-func (p *ProfileNameEqualTo) Check() (bool, error) {
+func (p *ProfileNameContains) Check() (bool, error) {
 	if p.Checked {
 		return p.Satisfied, nil
 	}
@@ -22,25 +22,27 @@ func (p *ProfileNameEqualTo) Check() (bool, error) {
 	if err != nil {
 		return false, fmt.Errorf("could not read apparmor profile: %w", err)
 	}
-	p.Satisfied = strings.Contains(string(profile), p.Name)
+	p.Satisfied = strings.Contains(string(profile), p.Expected)
 	p.Checked = true
 	return p.Satisfied, nil
 }
 
-func NewProfileNameEqualTo(name string) *ProfileNameEqualTo {
-	return &ProfileNameEqualTo{
+func NewProfileNameContains(name string) *ProfileNameContains {
+	return &ProfileNameContains{
 		BasePrerequisite: prerequisite.BasePrerequisite{
 			Name:   fmt.Sprintf("apparmor %s", name),
 			Info:   fmt.Sprintf("apparmor profile is %s", name),
 			ExeEnv: exeenv.InContainer,
 		},
-		Name: name,
+		Expected: name,
 	}
 }
 
 var (
 	// ProfileDockerDefault https://github.com/moby/moby/blob/v28.4.0/daemon/apparmor_default.go#L15
-	ProfileDockerDefault = NewProfileNameEqualTo("docker-default")
+	ProfileDockerDefault = NewProfileNameContains("docker-default")
 	// ProfileNerdctlDefault https://github.com/containerd/nerdctl/blob/v2.1.5/pkg/defaults/defaults_linux.go#L32
-	ProfileNerdctlDefault = NewProfileNameEqualTo("nerdctl-default")
+	ProfileNerdctlDefault = NewProfileNameContains("nerdctl-default")
+	// ProfileCriContainerd https://github.com/containerd/containerd/blob/v2.1.4/internal/cri/sputil/securityprofile_linux.go#L34
+	ProfileCriContainerd = NewProfileNameContains("cri-containerd.apparmor.d")
 )
--- prerequisite/apparmor/profile_test.go
+++ prerequisite/apparmor/profile_test.go
@@ -27,3 +27,43 @@ func TestE2E_ProfileDockerDefault(t *testing.T) {
 		assert.Equal(t, test.satisfied, satisfied)
 	})
 }
+
+func TestE2E_ProfileCriContainerd(t *testing.T) {
+	tests := map[string]struct {
+		satisfied bool
+	}{
+		"kubernetes-v1.33.1-calico-apparmor": {
+			satisfied: true,
+		},
+	}
+	testEnv := os.Getenv("TEST_ENV")
+	test, ok := tests[testEnv]
+	if !ok {
+		t.Skipf("Skipping test for unsupported environment: %s", testEnv)
+	}
+	t.Run(fmt.Sprintf("%s", testEnv), func(t *testing.T) {
+		satisfied, err := ProfileCriContainerd.Check()
+		assert.NoError(t, err)
+		assert.Equal(t, test.satisfied, satisfied)
+	})
+}
+
+func TestE2E_ProfileNerdctlDefault(t *testing.T) {
+	tests := map[string]struct {
+		satisfied bool
+	}{
+		"nerdctl-v2.1.2-apparmor": {
+			satisfied: true,
+		},
+	}
+	testEnv := os.Getenv("TEST_ENV")
+	test, ok := tests[testEnv]
+	if !ok {
+		t.Skipf("Skipping test for unsupported environment: %s", testEnv)
+	}
+	t.Run(fmt.Sprintf("%s", testEnv), func(t *testing.T) {
+		satisfied, err := ProfileNerdctlDefault.Check()
+		assert.NoError(t, err)
+		assert.Equal(t, test.satisfied, satisfied)
+	})
+}
