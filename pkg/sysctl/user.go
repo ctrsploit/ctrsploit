b69a5506e1deb79854f3356147714216cfdcb4f9
st0n3 <st0n3@debian.home>
2025-11-03 00:23:19 +0800

    feat: add pids cgroup support and enhance rlimit/sysctl utilities
    - Added `pkg/cgroup/pids` package to read and parse cgroup PID limits.
    - Introduced `New` function in rlimit to get resource limits by name.
    - Refactored sysctl functions:
      - Added support for `/proc/sys/kernel/threads-max`.
      - Improved handling of `pid_max` and `max_user_namespaces` with uint64 parsing.
      - Simplified file reading using internal helpers.

--- pkg/sysctl/user.go
+++ pkg/sysctl/user.go
@@ -9,21 +9,20 @@ import (
 
 const (
 	PathUserMaxUserNamespaces = "/proc/sys/user/max_user_namespaces"
-	UserNamespaceNotSupported = -1
 )
 
 /*
-MaxUserNamespaces returns -1 if file not exists, which means user namespace not supported
+MaxUserNamespaces returns 0 if file not exists, which means user namespace not supported
 */
-func MaxUserNamespaces() (int, error) {
+func MaxUserNamespaces() (uint64, error) {
 	content, err := os.ReadFile(PathUserMaxUserNamespaces)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return UserNamespaceNotSupported, nil
+			return 0, nil
 		}
 		return 0, err
 	}
-	i, err := strconv.Atoi(strings.TrimSpace(string(content)))
+	i, err := strconv.ParseUint(strings.TrimSpace(string(content)), 10, 64)
 	if err != nil {
 		return 0, fmt.Errorf("failed to parse user.max_user_namespaces: %w", err)
 	}
